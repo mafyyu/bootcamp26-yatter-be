aa21cda5d104d381623e60fd12b5d450fde824c6
mafyyu <morita0126m@gmail.com>
2026-08-04 16:20:56 +0900

    fix:migrateの修正

--- migrations/000004_create_yweet_table.up.sql
+++ migrations/000004_create_yweet_table.up.sql
@@ -1,8 +1,8 @@
 CREATE TABLE IF NOT EXISTS `yweet` (
     `id` varchar(255) NOT NULL,
-    `user_id` varchar(255) NOT NULL,
+    `user_id` bigint NOT NULL,
     `content` text NOT NULL,
     `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
-  CONS
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (`user_id`) REFERENCES `user`(`id`)
 );
