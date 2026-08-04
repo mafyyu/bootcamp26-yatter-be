e2da0315174f2c7d07b4329c6ffc9bde3ba10e03
mafyyu <morita0126m@gmail.com>
2026-08-04 16:09:06 +0900

    first commit

---
+++ migrations/000004_create_yweet_table.up.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS `yweet` (
+    `id` varchar(255) NOT NULL,
+    `user_id` varchar(255) NOT NULL,
+    `content` text NOT NULL,
+    `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+  CONS
+);
