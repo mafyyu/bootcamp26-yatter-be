63d5a4e65f8eec2029a54986d8688339f15a1084
mafyyu <morita0126m@gmail.com>
2026-08-04 22:56:57 +0900

    fix:migrationの型修正

--- migrations/000004_create_yweet_table.up.sql
+++ migrations/000004_create_yweet_table.up.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS `yweet` (
-    `id` varchar(255) NOT NULL,
+    `id` bigint NOT NULL,
     `user_id` bigint NOT NULL,
     `content` text NOT NULL,
     `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
